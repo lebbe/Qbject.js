7c17a631a4d017f78f7948a2a74cebd173764e94
Lars-Erik Bruce <lebchen@gmail.com>
2015-06-11 23:54:19 +0200

    simple script for compiling

---
+++ compile.sh
@@ -0,0 +1,2 @@
+uglifyjs src/Qbject.js src/manipulation.js src/traversal.js --compress unsafe --mangle > Qbject.min.js
+uglifyjs src/Qbject.js src/manipulation.js src/traversal.js --beautify > Qbject.js
