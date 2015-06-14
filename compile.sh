cfb70847703edf67ee0ad6eefd49948fc32e9724
Lars-Erik Bruce <lebchen@gmail.com>
2015-06-14 22:18:05 +0200

    report gzip size when compiling. ignore gzip file.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 uglifyjs src/Qbject.js src/manipulation.js src/traversal.js --compress unsafe --mangle > Qbject.min.js
 uglifyjs src/Qbject.js src/manipulation.js src/traversal.js --beautify > Qbject.js
+gzip Qbject.min.js -c > Qbject.min.js.gz
 cat Qbject.min.js | wc -c
+cat Qbject.min.js.gz | wc -c
