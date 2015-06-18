498080b521aaf501a2267c19054d214431d2fcf8
Lars-Erik Bruce <lebchen@gmail.com>
2015-06-18 22:16:04 +0200

    Dont save gzip-file, just count the bytes.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 uglifyjs src/Qbject.js src/manipulation.js src/traversal.js --compress unsafe --mangle > Qbject.min.js
 uglifyjs src/Qbject.js src/manipulation.js src/traversal.js --beautify > Qbject.js
-gzip Qbject.min.js -c > Qbject.min.js.gz
+
 cat Qbject.min.js | wc -c
-cat Qbject.min.js.gz | wc -c
+gzip Qbject.min.js -c | wc -c
+
